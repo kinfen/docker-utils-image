b2a273c62ed0c110cc9b2e98085efcd8f1f48d97
Kinfen <kinfen.in@gmail.com>
2018-01-30 10:25:12 +0800

    ftp build

--- jenkins/build.sh
+++ jenkins/build.sh
@@ -1,9 +1,8 @@
 #!/bin/bash
 
 set -x \
-	&& cd /home/www/kadm \
-	&& /usr/local/bin/npm install --prefer-offline \
+	&& /usr/local/bin/npm install /home/www/kadm --prefer-offline --unsafe-perm=true \
+	&& docker build --tag kinfen/kadm:app_last /home/www/kadm \
 	&& docker login -u kinfen -p deadkill \
-	&& docker build --tag kinfen/kadm:app_last . \
 	&& docker push kinfen/kadm:app_last \
 	&& docker service update --image kinfen/kadm:app_last mystack_apps
