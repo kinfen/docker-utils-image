4731d74673bb7b34e75d6ec76b28da6ecf58bf1a
Kinfen <kinfen.in@gmail.com>
2018-01-30 10:49:19 +0800

    privoxy build

--- build.sh
+++ build.sh
@@ -1,6 +1,9 @@
 #!/bin/bash
 
-UTILS="utils-lite utils-baseimage utils-vsftpd"
+UTILS="utils-lite
+	utils-baseimage
+	utils-vsftpd
+	utils-privoxy"
 
 for util in $UTILS; do
 	docker build ./$util -t kinfen/$util:latest
