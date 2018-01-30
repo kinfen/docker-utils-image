87b2dfaf12af49c294689d4d56810b02df45eaf7
Kinfen <kinfen.in@gmail.com>
2018-01-30 11:13:11 +0800

    jenkins build

--- build.sh
+++ build.sh
@@ -1,9 +1,11 @@
 #!/bin/bash
 
-UTILS="utils-lite \
-	utils-baseimage \
-	utils-vsftpd \
-	utils-privoxy"
+UTILS="utils-lite
+	utils-baseimage
+	utils-vsftpd
+	utils-privoxy
+	utils-mongo
+	utils-jenkins"
 
 for util in $UTILS; do
 	docker build ./$util -t kinfen/$util:latest
--- push.sh
+++ push.sh
@@ -4,7 +4,8 @@ UTILS="utils-lite \
 	utils-baseimage \
 	utils-vsftpd \
 	utils-privoxy
-	utils-baseimage"
+	utils-mongo
+	utils-jenkins"
 
 for util in $UTILS; do
 	docker push kinfen/$util:latest
