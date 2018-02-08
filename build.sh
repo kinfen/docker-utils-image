b36cfbfd66e747552b271311ff2c37a1c67a11b7
kinfen <kinfen.in@gmail.com>
2018-02-08 21:30:34 +0800

    test

--- build.sh
+++ build.sh
@@ -10,7 +10,7 @@
 # 	utils-ftp
 # 	utils-nodexm
 # 	utils-dockeragent"
-UTILS="utils-dockeragent"
+UTILS="utils-nodexm utils-dockeragent"
 
 for util in $UTILS; do
 	docker build ./$util -t kinfen/$util:latest
