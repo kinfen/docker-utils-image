ed52ba682db49367e5948c5d491c679a82c1c0e4
Kinfen <kinfen.in@gmail.com>
2018-09-11 15:15:54 +0800

    remove with pid

---
+++ utils-cnpm.org/config.js
@@ -0,0 +1,11 @@
+var config = {
+	scopes:"@kstudio",
+	admins: {
+	    admin: 'kinfen.in@gmail.com'
+	},
+	jsonLimit: "100mb",
+	syncModel:"exist",
+	syncInterval:"24h",
+	enableAbbreviatedMetadata:true
+};
+module.exports = config;
