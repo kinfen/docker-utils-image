e3aa86b2c6888ab2e71d9d21964b9b7e655d2c42
kinfen <kinfen.in@gmail.com>
2018-09-19 12:09:24 +0800

    to aliyun

---
+++ push_yun.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+ALIYUN_SERVER="registry.cn-shenzhen.aliyuncs.com"
+UTILS="utils-lite
+	utils-baseimage
+	utils-vsftpd
+	utils-privoxy
+	utils-mongo
+	utils-jenkins
+	utils-nginx
+	utils-ftp
+	utils-nodexm
+	utils-dockeragent
+	utils-redis
+	utils-mysql
+	utils-cnpm.org
+	utils-tomcat"
+# UTILS="utils-cnpm.org"
+
+for util in $UTILS; do
+	docker push $ALIYUN_SERVER/kstudio/$util:latest
+done
