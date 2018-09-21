7751c2960ab7e209e968092c770c577830d86ac0
kinfen <kinfen.in@gmail.com>
2018-09-22 01:22:49 +0800

    t

--- utils-gogs/server.sh
+++ utils-gogs/server.sh
@@ -1,5 +1,13 @@
 #!/bin/sh
-set -x \
-    echo "gogs is going to start" \
-    && gogs web -c ~/app.ini
+set -x
+set -e
+setids() {
+    PUID=${PUID:-1000}
+    PGID=${PGID:-1000}
+    groupmod -o -g "$PGID" $GOGS_USER
+    usermod -o -u "$PUID" $GOGS_USER
+}
+setids
+echo "gogs is going to start" \
+&& gogs web -c ~/app.ini
     
