24c6eb211b1644d3974216bf7a3a13d9e24c245f
kinfen <kinfen.in@gmail.com>
2018-09-22 01:47:53 +0800

    test

--- utils-gogs/server.sh
+++ utils-gogs/server.sh
@@ -1,13 +1,6 @@
 #!/bin/sh
 set -x
 set -e
-setids() {
-    PUID=${PUID:-1000}
-    PGID=${PGID:-1000}
-    groupmod -o -g "$PGID" $GOGS_USER
-    usermod -o -u "$PUID" $GOGS_USER
-}
-setids
 echo "gogs is going to start" \
 && gogs web -c ~/app.ini
     
