0aa793a5f9060d6f530d5768dee605e42b2d4f69
kinfen <kinfen.in@gmail.com>
2018-09-21 23:50:03 +0800

    s

--- utils-gogs/server.sh
+++ utils-gogs/server.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 set -x \
     echo "gogs is going to start" \
-    && sed -i "s/RUN_USER = gogs/RUN_USER=git/" ${GIT_HOME}/app.ini
+    && sed -i "s/RUN_USER = gogs/RUN_USER=git/" ${GIT_HOME}/app.ini \
     && su - git && gogs web -c ~/app.ini
     
