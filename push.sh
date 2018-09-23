9866788119adcbcac6af3acb570defd9b2cd4750
gzgu_user1 <gzgi@gzgi.com>
2018-09-23 23:04:52 +0800

    cnpm rebuild

--- push.sh
+++ push.sh
@@ -15,7 +15,7 @@ ALIYUN_SERVER="registry.cn-shenzhen.aliyuncs.com/kstudio"
 #	utils-cnpm.org
 #	utils-tomcat
 	# utils-gogs"
-UTILS="utils-gogs"
+UTILS="utils-cnpm.org"
 
 for util in $UTILS; do
 	docker tag $ALIYUN_SERVER/$util:latest kinfen/$util:latest
