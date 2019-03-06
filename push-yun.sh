0ad2d31211a2cb039bf5d7b0b52e351d3d42ff38
Kinfen <kinfen.in@gmail.com>
2019-03-06 15:15:58 +0800

    update ngrok image

--- push-yun.sh
+++ push-yun.sh
@@ -16,7 +16,7 @@ ALIYUN_SERVER="registry.cn-shenzhen.aliyuncs.com/kstudio"
 #	utils-tomcat
 	# utils-ngrok
 	# utils-gogs"
-UTILS="utils-gogs"
+UTILS="utils-ngrok"
 
 for util in $UTILS; do
 	docker push $ALIYUN_SERVER/$util:latest
