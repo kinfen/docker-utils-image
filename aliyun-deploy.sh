6c5e60594ba6bf76e16c0ba46ad21bf6976ca3d4
Kinfen <kinfen.in@gmail.com>
2019-05-14 19:02:36 +0800

    update

--- aliyun-deploy.sh
+++ aliyun-deploy.sh
@@ -1,5 +1,3 @@
 #!/bin/bash
-echo 'ready to login docker'
-docker login --username=$YUN_USERNAME --password=$YUN_PASSWORD registry.cn-shenzhen.aliyuncs.com
-sh build-yun.sh
-sh push-yun.sh
+echo 'ready to use svn'
+svn help
